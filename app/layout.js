7f3fe4182a89937e1b79e3b61ce6a5a0024ba0cd
amymb1980 <amymbarnes@gmail.com>
2025-07-03 12:14:07 +1000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
-import './globals.css'
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-50">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
