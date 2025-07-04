8379109259bbce6b68fe0766d7dece4c6cfdf7ec
amymb1980 <amymbarnes@gmail.com>
2025-07-04 14:11:55 +1000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,19 +2,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
-        <script>
-          tailwind.config = {
-            theme: {
-              extend: {
-                colors: {
-                  'caption-orange': '#EA8953',
-                  'caption-green': '#007B40'
-                }
-              }
-            }
-          }
-        </script>
+        <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body>{children}</body>
     </html>
