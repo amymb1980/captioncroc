8487aa2c2421ed130b2fb258b57c892d3286ceb1
amymb1980 <amymbarnes@gmail.com>
2025-07-05 19:13:32 +1000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://cdn.tailwindcss.com/3.3.0"></script>
+        {/* No CSS at all for testing */}
       </head>
       <body>{children}</body>
     </html>
