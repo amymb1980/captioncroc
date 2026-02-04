b59720c7b0365e1e37e8955ad1edd90bcf9b0f39
amymb1980 <amymbarnes@gmail.com>
2026-02-04 14:19:30 +1000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/site.webmanifest" />
+       
         
         {/* Theme color for mobile browsers */}
         <meta name="theme-color" content="#007B40" />
